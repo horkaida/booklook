3a94e59416cd4c04a6ee7a42b2ef662a668f6ddd
olenalipinska <eveforelinska@gmail.com>
2023-11-13 22:00:24 +0200

    Celery add, calc_average_book_rate func add as periodical task

--- books/tasks.py
+++ books/tasks.py
@@ -1,16 +1,10 @@
-from celery import app
-from celery.schedules import crontab
+
+from __future__ import absolute_import, unicode_literals
+from celery import shared_task
 from books import models
-from django.db.models import Count
-
-CELERYBEAT_SCHEDULE = {
-   'calc_average_book_rate': {
-       'task': 'calc_average_book_rate',
-       'schedule': crontab(minute='1')}
-}
 
 
-@app.task(name='calc_average_book_rate')
+@shared_task
 def calc_average_book_rate():
     all_rates = models.Rate.objects.all()
     all_books = models.Book.objects.all()
@@ -18,12 +12,11 @@ def calc_average_book_rate():
         rates_summ = 0
         rates_count = 0
         for rate in all_rates:
-            if book.id == rate.book_id:
+            if book.id == rate.book_id_id:
                 rates_summ += rate.rate
                 rates_count += 1
         if rates_count:
             models.Book.objects.filter(id=book.id).update(average_rate=rates_summ/rates_count)
         else:
             models.Book.objects.filter(id=book.id).update(average_rate=0)
-    print('CELERY WORKED!!!!')
 
