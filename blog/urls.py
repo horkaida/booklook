10a58df7ac8bf3ad8c4a950cbaaccbd186545cd4
olenalipinska <eveforelinska@gmail.com>
2023-11-14 18:48:22 +0200

    wip

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 
 urlpatterns = [
     path('', views.get_all_posts, name='get_all_posts'),
+    path('<post_id>', views.get_one_post, name='get_one_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,12 @@ from django.shortcuts import render
 
 def get_all_posts(request, page_number=1):
     all_posts = models.BlogPost.objects.all().order_by('-date')
-    print('pos', all_posts)
     page_number = request.GET.get('page') if request.GET.get('page') else page_number
     page_obj = paginate(page_number=page_number, data=all_posts, per_page=12)
-    print('sdasd', page_obj)
     return render(request, 'blog/index.html', {'page_obj':page_obj})
 
+
+def get_one_post(request, post_id):
+    post = models.BlogPost.objects.filter(id=post_id).first()
+    return render(request, 'blog/post.html', {'post':post})
 
